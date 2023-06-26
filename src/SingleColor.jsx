71e7d5f5d7a2b4ae092c4b10aa20d1cb7b63d995
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-06-26 17:32:11 +0530

    toast library added

---
+++ src/SingleColor.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SingleColor = ({ color, index }) => {
+    const { hex, weight } = color;
+   
+  return (
+      <article className={index>10?'color color-light':'color'} style={{ backgroundColor: `#${hex}` }}>
+          <p className='percent-value'>{weight}%</p>
+          <p className='percent-value'>#{hex}</p>
+    </article>
+  )
+}
+
+export default SingleColor
