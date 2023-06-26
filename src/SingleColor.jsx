eb37ae109d1dc00aca61bb436f8e429a50243d16
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-06-26 18:18:18 +0530

    clipboard added

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
+import { toast } from "react-toastify";
 
 const SingleColor = ({ color, index }) => {
-    const { hex, weight } = color;
-   
-  return (
-      <article className={index>10?'color color-light':'color'} style={{ backgroundColor: `#${hex}` }}>
-          <p className='percent-value'>{weight}%</p>
-          <p className='percent-value'>#{hex}</p>
-    </article>
-  )
+  const { hex, weight } = color;
+const saveToClipboard =async () => {
+    if (navigator.clipboard) {
+        try {
+            navigator.clipboard.writeText(`#${hex}`)
+            toast.success("Color copied to clipboard")
+        } catch (error) {
+            toast.error("failed to copy color to clipboard")
+        }
+    } else {
+        toast.error("clipboard access not available")
+    }
 }
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+          style={{ backgroundColor: `#${hex}` }}
+          onClick={saveToClipboard}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="percent-value">#{hex}</p>
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
